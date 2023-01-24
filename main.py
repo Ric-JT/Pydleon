bfa905fb77e6e15767985b2e598d20cd74ca51a8
RicJT <ricardo.j.trancoso@gmail.com>
2023-01-24 23:57:04 +0000

    Add Widget base class

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ import curses
 
 VERSION = "v PROTOTYPE_02"
 
+class MyWidget:
+	def __init__(self,app,parent=None):
+		self.app = app
+		self.parent = parent
+		self.winlist = []
+	def run(self,key):
+		raise NotImplementedError()
+
+	def refresh(self):
+		raise NotImplementedError()
+
